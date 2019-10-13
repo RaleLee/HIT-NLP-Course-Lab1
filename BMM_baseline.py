695a2d393cfba000c561fcf1bba81cb54a9dc4f5
RaleLee <1367845376@qq.com>
2019-10-13 23:04:12 +0800

    Finish section 3.3. The hashset in 3.4 is a rubbish.

---
+++ BMM_baseline.py
@@ -0,0 +1,59 @@
+# coding=utf-8
+from utils import readFile, writeSeg, binary_search
+import time
+
+def BMM(dicpath, textpath):
+    # read text from file
+    textlines = readFile(textpath)
+
+    # read dic from file
+    oriDic = readFile(dicpath)
+    # new a dic for running code and the origin dic won't be destroy
+    dic = []
+    # count the maxlen of a word
+    maxWordLen = 0
+    dicSize = len(oriDic)
+    for i in range(dicSize):
+        tmp = oriDic[i].split(" ") # the dic format: word POS times
+        dic.append(tmp[0]) # only needs word
+        if(len(tmp[0]) > maxWordLen):
+            maxWordLen = len(tmp[0])
+    dic =  set(dic)
+    # count time
+    startTime = time.time()
+    # Do FMM
+    textSize = len(textlines)
+    seg = []
+    # count = 0
+    for i in range(textSize):
+        text = textlines[i].strip()
+        wordList = []
+        while len(text) > 0:
+            # True_statements if expression else False_statements
+            lenTryWord = maxWordLen if len(text) > maxWordLen else len(text) 
+            tryWord = text[len(text) - lenTryWord:]
+            while tryWord not in dic:
+                if len(tryWord) == 1:
+                    break
+                tryWord = tryWord[1:]
+            # match successfully
+            wordList.insert(0, tryWord)
+            # start match the remain part
+            text = text[:len(text) - len(tryWord)]
+        seg.append(wordList)
+    endTime = time.time()
+    print((endTime - startTime) * 1000)
+    return seg 
+
+
+
+dicpath = "dic.txt"
+textpath = "199801_sent.txt"
+segpath = "seg_BMM.txt"
+def main():
+    seg = BMM(dicpath, textpath)
+    writeSeg(segpath, seg)
+    return
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
+# coding=utf-8
 def readFile(path):
     with open(path, 'r') as f:
         lines = f.readlines()
     f.close()
     return lines
 
+# use to write dic in part 3.1
 def writeDic(path, dic):
     with open(path, 'w') as f:
         for word in dic:
@@ -12,6 +14,7 @@ def writeDic(path, dic):
     f.close()
     return 
 
+# use to write seg result in part 3.2
 def writeSeg(path, lines):
     with open(path, 'w') as f:
         for line in lines:
@@ -21,8 +24,17 @@ def writeSeg(path, lines):
             f.write(newline)
             #print(newline)
             f.write("\n")
-    f.close
+    f.close()
     return
+
+# use to write analysis result in part 3.3
+def writeList(path, lines):
+    with open(path, 'w') as f:
+        for line in lines:
+            f.write(line)
+            f.write("\n")
+    f.close()
+    return 
 
 def binary_search(slis, key):
     left = 0
